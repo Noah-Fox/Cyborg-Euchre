440e0840a9f45866c69d7a3e375fa994b24c0a1e
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-03-28 14:32:18 -0400

    Update backend

--- mediator-backend/app.js
+++ mediator-backend/app.js
@@ -9,16 +9,19 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
     if (!req.query.arg){
         res.status(200).send("please give arg");
+        console.log(`Called with no arg`);
         return;
     }
     execFile('../hello', [req.query.arg], (error, stdout, stderr) => {
         if (error) {
             throw error;
         }
-        res.status(200).send(stdout);
+        res.status(200).json({ans: stdout});
     })
+    console.log(`Called with ${req.query.arg}`);
 });
 
 app.get('/goodbye', (req, res) => {
